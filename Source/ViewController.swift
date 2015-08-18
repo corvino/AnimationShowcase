c7a80f4fa991a62d0efe4e1025da6dbf198de244
Nathan Corvino <nathan@corvino.com>
2015-08-18 14:17:21 -0600

    Restructure project.

---
+++ Source/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  AnimationShowcase
+//
+//  Created by Nathan Corvino on 8/17/15.
+//  Copyright (c) 2015 Nathan Corvino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+}
+
